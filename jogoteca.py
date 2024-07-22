add0df38cc884cb09dfda19c53821b58a2806f20
Orlando <cursosopsf@gmail.com>
2024-07-22 09:37:59 -0300

    refatorando o código

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+class Jogo():
+    def __init__(self, nome, categoria, console):
+        self._nome=nome
+        self._categoria = categoria
+        self._console = console
+
+jogo1 = Jogo('xadrez','tabuleiro','pau')
+jogo2 = Jogo('dama','tabuleiro','pc')
+jogo3 = Jogo('futebol', 'equipe',  'campo')
+lista = [jogo1, jogo2,jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    lista = ['futebol','xadrez','dama']
+@app.route('/')
+def index():    
     return render_template('lista.html', titulo='JOGO', jogos=lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html')
+
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return redirect('/')
+
+
+app.run(debug=True)
